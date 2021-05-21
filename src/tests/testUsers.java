ee35adae20703affeeb005699f30aeeebc114a84
nathesloth <natalie_smith422@outlook.com>
2021-05-21 22:39:57 +1000

    Update test users

--- src/tests/testUsers.java
+++ src/tests/testUsers.java
@@ -1,155 +1,134 @@
+// 	******************************************************************************************
+// 	**																        **
+// 	**	Filename: testUsers.java									             **
+// 	**																	   **
+// 	**	Description: Testing Code for User Class							   **
+// 	**													   	                  **
+// 	**																	   **
+// 	**	Contributors: Natalie Smith (n10462040)									   **
+// 	**																	   **
+// 	**																        **
+// 	**	Date Created: 15/05/2021												   **
+// 	**																	   **
+// 	**																	   **
+// 	**	Change Documentation											        **
+// 	**		> Updated Version										        **
+// 	**																        **
+// 	**																	   **
+// 	**																	   **
+// 	******************************************************************************************
+
+
 package tests;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import user.User;
+import tradingPlatform.user.UserType;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.*;
+public class testUsers {
+    // Test 0: Declaring User objects for testing purposes
+    User user;
+    User user1;
+    User user2;
 
-import tests.UserException;
-
-import tradingPlatform.user.User;
-
-enum UserType {
-    Employee,
-    Admin,
-    Lead
-}
-
-public class testUsers{
-    /* Test 1: Testing whether the user exists */
     @BeforeEach @Test
     public void setUpUsers(){
-//        users = new User();
+        user = new User();
+        user1 = new User("neo123", "Patrick", "Star", 4, "password123", UserType.Admin);
+        user2 = new User("mik434", "Michael", "Jackson", 2, "password456", UserType.Employee);
     }
 
-    /* Test 2: Adding new user to database. Also tests that valid values inputted. */
+    // Test 0 : Base case ID
     @Test
-    public void testNewUser() throws UserException {
-        User testUser= new User("neo123", "Patrick", "Star", 4, "password123", UserType.Admin);
-//        users.addUser(testUser);
-//        assertEquals("blahblah123", getUser("blahblah123"), "Adding new user failed");
+    public void baseUserID(){
+        assertEquals(0, user._username);
     }
 
-    /* Test : Used to determine if invalid inputs can still be parsed to the database. */
+    // Test 1: Adding same user to database.
+    @Test
+    public void duplicateUser() throws UserException {
+        user = new User("neo123", "Patrick", "Star", 4, "password123", UserType.Admin);
+        assertEquals("neo123", user._username, "Adding same user failed");
+    }
+
+    // Test 2: Determine if invalid inputs can still be parsed to the database.
     @Test
     public void testInvalidInputs() throws UserException {
-        User testUser = new User(1,2,3,"",5,6);
+        user = new User(1,2,3,"",5,6);
     }
 
-    /* Test : Used to see if user can input no password */
+    // Test 3: User input no password
     @Test
     public void testNoPassword() throws UserException {
-        User testUser = new User("neo123", "Patrick", "Star", 4, "", UserType.Admin);
+        user = new User("neo123", "Patrick", "Star", 4, "", UserType.Admin);
     }
 
-    /* Test 3: Logging in user who doesn't exist */
+
+    // Test 4: User input empty password
     @Test
-    public void findUser() throws UserException {
-
+    public void testEmptyPassword() throws UserException {
+        user = new User("neo123", "Patrick", "Star", 4, , UserType.Admin);
     }
 
-    /* Test 4: Determine if the user has permission to add user */
+    // Test 5: Test that user belongs to valid unit ID
     @Test
-    public void testPermissions() throws PermissionException {
-
+    public void userValidID() {
+        assertEquals("mik434", user2._username);
     }
 
-    /* Test 5: Can't add user who already exists - is unique */
+    // Test 6: Test that the firstname and lastname match the user ID
     @Test
-    public void duplicateUser() {
-<<<<<<< HEAD:src/tests/testsUsers.java
-        assertThrows(UserException, () -> {
-            addUser("blahblah123");
-            addUser("blahblah123");
-        })
-=======
-        assertThrows(UserExceptions, () -> {
-            users.addUser("blahblah123");
-            users.addUser("blahblah123");
-        });
->>>>>>> f16c823d7f90eb00a590eeb41c0ffc4245f37b9c:src/tests/testUsers.java
+    public void userNameMatch() {
+        assertEquals("Michael", user2._firstName);
+        assertEquals("Jackson", user2._lastName);
     }
 
-    /* Test 6: Test that user always has a password */
+    // Test 7: User is only assigned to valid accountType
     @Test
-    public void passwordAssigned() {
+    public void userValidAcType() throws UserException {
+        user.setUserType(UserType.Intern);
     }
 
-    /* Test 7: Test that user belongs to valid unit ID */
+    // Test 8: Get credits returns actual credits of unit
     @Test
-    public void userHasID() {
-        assertThrows();
+    public void userCreditMatch() {
+        assertEquals(300, user2.getCredits());
     }
-    /* Test 8: User has names */
+
+    // Test 9: Test user always has name
     @Test
-    public void userHasID() {
-        assertThrows();
+    public void userSameName(){
+        assertEquals("Harry", user2._firstName);
+        assertEquals("Jackson", user2._firstName);
     }
 
-    /* Test 9: User is only assigned to valid accountType */
+    /* Test 10: User able to change password */
     @Test
-    public void userHasID() {
-        assertThrows();
+    public void userChangePassword() {
+        user1.changePassword("maxwell");
+        assertEquals("maxwell", user1.password);
     }
 
-    /* Test 10: User is only assigned to valid accountType */
-    @Test
-    public void userHasID() {
-        assertThrows();
-    }
-
-    /* Test 11: Get credits returns actual credits of unit */
-    @Test
-    public void userHasID() {
-        assertThrows();
-    }
-
-    /* Test 12: Check GUI matches the appropritate accountType */
-    @Test
-    public void userHasID() {
-        assertThrows();
-    }
-
-    /* Test 13: Test that the firstname and lastname match the user ID */
-    @Test
-    public void userHasID() {
-        assertThrows();
-    }
-
-    /* Test 14: Test credits matches updated credit balance of unit */
-    @Test
-    public void userHasID() {
-        assertThrows();
-    }
-
-    /* Test 15: User able to change password if forgotten */
-    @Test
-    public void userHasID() {
-        assertThrows();
-    }
-
-    /* Test 16: User able to change password */
-    @Test
-    public void userHasID() {
-        assertThrows();
-    }
-
-    /* Test 17: User is provided notifications relevant to them */
-    @Test
-    public void userHasID() {
-        assertThrows();
-    }
-
-    /* Test 18: Can add new IT admin users */
-    /* Test 19: IT Admin given special GUI */
-    /* Test 19: Only IT Admin given special permissions to add user */
-    /* Test 19: Only IT Admin given special permissions to add asset */
-    /* Test 19: Only IT Admin given special permissions to add unit */
-    /* Test 19: Only IT Admin given special permissions to edit asset */
-    /* Test 19: Only IT Admin given special permissions to view requests */
-    /* Test: IT admin can view requests */
 
 
-    /* Test : Login is filled */
-    /* Test : Login username exists */
-    /* Test : Login password matches the username */
+    // Test 10: Test credits matches updated credit balance of unit
+//    @Test
+//    public void userHasID() {
+//        assertThrows();
+//    }
+
+
+    // Additional
+
+
+    // The graveyard - tests that I don't know what to do about
+
+    // Test 6: Test that user always has a password
+//    @Test
+//    public void passwordAssigned() {
+//        assertEquals("password456", user2._password);
+//    }
 }
