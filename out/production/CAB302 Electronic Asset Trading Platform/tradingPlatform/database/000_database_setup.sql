2436270ab6c59949cccdbf7dd01dcb72ff2c111b
Jeremy Chang <n10462040@qut.edu.au>
2021-05-24 01:59:43 +1000

    Modified Databse Configuration

---
+++ out/production/CAB302 Electronic Asset Trading Platform/tradingPlatform/database/000_database_setup.sql
@@ -0,0 +1,67 @@
+CREATE DATABASE tradingPlatform;
+
+USE tradingPlatform;
+
+CREATE TABLE users (
+    userID          CHAR(10)            PRIMARY KEY     NOT NULL    ,
+    userName        VARCHAR(144)                        NOT NULL    ,
+    firstName       VARCHAR(144)                        NOT NULL    ,
+    lastName        VARCHAR(144)                        NOT NULL    ,
+    unitID          CHAR(10)                                        ,
+    accountType     VARCHAR(64)                                     ,
+    password        VARCHAR(244)                        NOT NULL             
+);
+
+CREATE TABLE units (
+    unitID          CHAR(10)            PRIMARY KEY     NOT NULL    ,
+    unitName        VARCHAR(144)                        NOT NULL    ,
+    creditBalance   FLOAT                                           ,
+    creditLimit     FLOAT                                       
+);
+
+CREATE TABLE assets (
+    assetID         CHAR(10)            PRIMARY KEY     NOT NULL    ,
+    assetName       VARCHAR(64)                         NOT NULL    ,
+    currentPrice    FLOAT                                           ,
+    assetType       VARCHAR(64)
+);
+
+CREATE TABLE inventory (
+	unitID          CHAR(10)            PRIMARY KEY     NOT NULL    ,
+    assetID         CHAR(10)             				NOT NULL    ,
+    quantity		INT(8)                                          ,
+	orderID         CHAR(10)
+);
+
+CREATE TABLE orders (
+    orderID         CHAR(10)            PRIMARY KEY     NOT NULL    ,
+    userID          CHAR(10)                            NOT NULL    ,
+    unitID          CHAR(10)                            NOT NULL    ,
+    orderTime       DATETIME                                        ,
+    assetID         CHAR(10)                            NOT NULL    ,
+    orderStatus     VARCHAR(32)                                     ,
+    orderType       VARCHAR(32)                                     ,
+    orderPrice      FLOAT                               NOT NULL    ,
+    orderQuantity   INT(16)                             NOT NULL    ,
+    quantFilled     INT(16)                                         ,
+    quantRemain     INT(16)                                     
+);
+
+CREATE TABLE requests (
+	requestID       CHAR(10)            PRIMARY KEY     NOT NULL    ,
+    userID          CHAR(10)              			    NOT NULL    ,
+    requestType	    VARCHAR(144)                                    ,
+	requestMessage	VARCHAR(512)
+);
+
+CREATE TABLE notifications (
+    notifID         CHAR(10)            PRIMARY KEY     NOT NULL    ,
+    userID          CHAR(10)                            NOT NULL    ,
+    userMessage     VARCHAR(244)
+);
+
+CREATE TABLE watchlists (
+	unitID          CHAR(10)                            NOT NULL    ,
+	assetID         CHAR(10)                            NOT NULL    ,
+    CONSTRAINT      pk_watchlists       PRIMARY KEY (unitID, assetID)
+);
--- src/tradingPlatform/Main.java
+++ src/tradingPlatform/Main.java
@@ -19,7 +19,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
-	// write your code here
-        connection = (Connection) new JBDCConnection();
+        // write your code here
+        new JBDCConnection();
+
     }
 }
