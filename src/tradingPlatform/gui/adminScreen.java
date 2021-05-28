505f48357c18a6585e3d4ff2379e98a9d72d2444
nathesloth <natalie_smith422@outlook.com>
2021-05-29 08:33:37 +1000

    Update admin screen methods

--- src/tradingPlatform/gui/adminScreen.java
+++ src/tradingPlatform/gui/adminScreen.java
@@ -5,8 +5,15 @@ import tradingPlatform.Main;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.sql.SQLException;
 
+/**
+ * Initiates the user interface for the Admin screen. Can only be called when the usertype
+ * is found to be an admin. Listeners are also initiated within this class to ensure
+ * the relevant buttons are being used.
+ */
 public class adminScreen extends Screen {
     private JFrame frame;
     private JPanel panel;
@@ -27,21 +34,50 @@ public class adminScreen extends Screen {
     private ImageIcon logoutIconS = new ImageIcon("src/img/logoutPress-01.jpg");
 
     public adminScreen() throws SQLException {
+        initUI();
+        addButtonListeners();
+        frame.addWindowListener(new ClosingListener());
+
+    }
+
+
+    /**
+     * Initialises the panels used and calls the relative methods to set up the frame.
+     * Would essentially be the main code that is used to create each of the elements
+     * within the frame. Sets the main panel to a preferred dimension and also initialises
+     * the sidebar buttons.
+     */
+    private void initUI(){
         // Setting up the frame and panels
-        frame = new JFrame();
         panel = new JPanel();
         logoutPane = new JPanel();
         sidebarPanel = new JPanel();
+        panel.setPreferredSize(new Dimension(1380, 1000));
 
         adminSidebar();
+        setupAdminFrame();
+    }
 
-        sidebarPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
-        sidebarPanel.setLayout(new BoxLayout(sidebarPanel, BoxLayout.Y_AXIS));
-//        panel.setBorder(BorderFactory.createEmptyBorder(400, 400, 400, 970));
-        panel.setPreferredSize(new Dimension(1380, 1000));
-        sidebarPanel.setPreferredSize(new Dimension(310, 1000));
-//        panel = new requestGUI();
-        logoutPane.setBorder(BorderFactory.createEmptyBorder(500, 0, 0, 0));
+    /**
+     * Adds the button listeners relevant to the admin user.
+     */
+    private void addButtonListeners(){
+        logoutButton.addActionListener(this);
+    }
+
+
+    /**
+     * Makes a JFrame that would initialise and set up the Admin UI window. This
+     * code is purely to create and setup up the frame and panels. The frame would
+     * add the main sidebar panel - with BorderLayout - to keep on the WEST of the frame,
+     * whilst the main panel (a.k.a. panel) would be set up to sit on the east of the frame.
+     * The title of the window is set to Admin, and all of the data would be packed to return
+     * a frame to the main constructor.
+     * @return a frame containing each the main panel and sidebar panel
+     */
+    private JFrame setupAdminFrame(){
+        // Initialize the frame for the admin UI
+        frame = new JFrame();
 
         // Adding the panes to the final sidebar frame
         frame.add(panel, BorderLayout.EAST);
@@ -49,12 +85,18 @@ public class adminScreen extends Screen {
         frame.setTitle("Venda - Admin");
         frame.pack();
         frame.setVisible(true);
-
-        logoutButton.addActionListener(this);
+        return frame;
     }
 
 
     private void adminSidebar(){
+        sidebarPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+        sidebarPanel.setLayout(new BoxLayout(sidebarPanel, BoxLayout.Y_AXIS));
+//        panel.setBorder(BorderFactory.createEmptyBorder(400, 400, 400, 970));
+        sidebarPanel.setPreferredSize(new Dimension(310, 1000));
+//        panel = new requestGUI();
+        logoutPane.setBorder(BorderFactory.createEmptyBorder(500, 0, 0, 0));
+
         // Resizing the size of Main Logo
         Image mainImg = mainIcon.getImage();
         Image mainScale = mainImg.getScaledInstance(200, 150, Image.SCALE_SMOOTH);
@@ -95,6 +137,13 @@ public class adminScreen extends Screen {
             Main.resetCurrentUser();
             new loginGUI();
             frame.dispose();
+        }
+    }
+
+
+    private class ClosingListener extends WindowAdapter {
+        public void windowClosing(WindowEvent e) {
+            System.exit(0);
         }
     }
 }
