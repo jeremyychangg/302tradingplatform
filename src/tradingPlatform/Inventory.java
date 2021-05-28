435b6ff933f9213c0c5baa29f5365b28bc2ccc55
nathesloth <natalie_smith422@outlook.com>
2021-05-29 08:35:52 +1000

    Inventory update

--- src/tradingPlatform/Inventory.java
+++ src/tradingPlatform/Inventory.java
@@ -1,10 +1,38 @@
+// 	******************************************************************************************
+// 	**																        **
+// 	**	Filename: Inventory.java									             **
+// 	**																	   **
+// 	**	Description: Inventory Class						   **
+// 	**													   	                  **
+// 	**																	   **
+// 	**	Contributors: Natalie Smith (n10524215)									   **
+// 	**																	   **
+// 	**																        **
+// 	**	Date Created: 16/05/2021												   **
+// 	**																	   **
+// 	**																	   **
+// 	**	Change Documentation											        **
+// 	**		> Updated version										        **
+// 	**																        **
+// 	**																	   **
+// 	**																	   **
+// 	******************************************************************************************
+//
 package tradingPlatform;
 
 import java.util.ArrayList;
 
 public class Inventory {
+    private int unitID;
+    private int assetID;
+    private int quantity;
+    private int orderID;
 
-    public Inventory(String unitID, String assetID, int quantity) {
+    public Inventory(int unitID, int assetID, int quantity, int orderID){
+        this.unitID = unitID;
+        this.assetID = assetID;
+        this.quantity = quantity;
+        this.orderID = orderID;
     }
 
     public Inventory(String unitID, String assetID, int quantity, int orderID) {
