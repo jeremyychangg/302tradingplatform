7bed625b41af29d9d3b82cecd295d86849b43b77
nicoletruong <nicoletruong01@gmail.com>
2021-06-01 23:24:57 +1000

    Added new tests for parameters

--- src/tests/testUnit.java
+++ src/tests/testUnit.java
@@ -14,28 +14,82 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class testUnit {
-    Unit unit;
+    Unit unit1;
+    Unit unit2;
+
+    @BeforeEach
+    @Test
+    void setupUnit() {
+        // Unit with all fields
+        unit1 = new Unit("IT00000010","IT", 20000.00, 10000.00);
+        // Unit without unit ID
+        unit2 = new Unit("Human Resources", 8050.28, 5000.00);
+    }
+    // Construct test list of users
+    @BeforeEach
+    @Test
+    public List<Integer> userArray() {
+        List<tradingUser> userList = new ArrayList<String>();
+        userList.add("S0001");
+        userList.add("L0064");
+        userList.add("A0002");
+        return userList;
+    }
+
+    @Test
+    // - Test 1: Check that the userID provided is a string
+    public void unitIDIsString() {
+        assertTrue(unit1.getUnitID() == "IT00000010");
+    }
+
+    @Test
+    // - Test 2: Check that the userID provided is a string
+    public void unitNameIsString() {
+        assertTrue(unit1.getUnitName() == "IT");
+        assertTrue(unit2.getUnitName() == "Human Resources");
+    }
+
+    @Test
+    // - Test 3: Check that the credit balance provided is a double
+    public void CBIsString() {
+        assertTrue(unit1.getCreditBalance() == 20000.00);
+        assertTrue(unit2.getCreditBalance() == 8050.28);
+    }
+
+    @Test
+    // - Test 4: Check that the limit provided is a double
+    public void limitValid() {
+        assertTrue(unit1.getLimit() == 10000);
+        assertTrue(unit2.getLimit() == 5000.00);
+    }
+
+    @Test
+    // - Test 5: Check that the credit balance is not less than the limit
+    public void moreCreditThanLimit() {
+        assertTrue(unit1.getCreditBalance() >= unit1.getLimit());
+        assertTrue(unit2.getCreditBalance() >= unit2.getLimit());
+    }
+
+    @Test
+    // - Test 6: Check that the credit balance is not negative
+    public void sufficientCredit {
+        assertTrue(unit1.getCreditBalance() >= 0.00);
+        assertTrue(unit2.getCreditBalance() >= 0.00);
+    }
+
+    @Test
+    // - Test 7: Check that the limit is not negative
+    public void sufficientLimit {
+        assertTrue(unit1.getLimit() >= 0.00);
+        assertTrue(unit2.getLimit() >= 0.00);
+    }
+
+    // * needs to be completed * //
 
     // check that the list is not empty
     // check that the list returns a list of users
     // check when you add a unit
     // check when you remove a unit
-    // check parameters
-    // check that the creditBalance is not less than the limit
-    // check that the creditBalance is not negative
-
-    public List<Integer> userArray() {
-        List<tradingUser> userList = new ArrayList<String>();
-        userList.add("IT00000010");
-        return userList;
-    }
-
-    // - Test 1: check if the unit exists
-    @BeforeEach
-    @Test
-    public void setup() {
-        unit = new Unit();
-    }
 
     @Test
     public void testMyArray() {
@@ -43,7 +97,7 @@ class testUnit {
         assertEquals(Arrays.asList("IT00000010"), testUnit.userList);
     }
 
-    // - Test 2: check if the unit returns a list
+    // - Test :
     @Test
     public void noList() {
         Unit testCases = new Unit();
@@ -56,13 +110,7 @@ class testUnit {
 
     }
 
-    // - Test 4: check if the list of users contains all integers
-    @Test
-    public void notIntegers() {
-
-    }
-
-    // - Test 5: check if the list of users is not full of strings
+    // - Test 5: check if the list of users is full of strings
     @Test
     public void allStrings() {
 
