3cbd19ef6803abdce755d12032693f23c8f7f34a
Jeremy Chang <n10462040@qut.edu.au>
2021-06-06 12:26:19 +1000

    Commented Exceptions

--- src/tradingPlatform/exceptions/InvalidAssetException.java
+++ src/tradingPlatform/exceptions/InvalidAssetException.java
@@ -1,6 +1,33 @@
+// 	******************************************************************************************
+// 	**																						**
+// 	**	Filename: InvalidAssetException.java  												**
+// 	**																						**
+// 	**	Description:																		**
+// 	**																						**
+// 	**																						**
+// 	**	Contributors: Jeremy Chang															**
+// 	**																						**
+// 	**																						**
+// 	**	Date Created:																		**
+// 	**																						**
+// 	**																						**
+// 	**	Change Documentation																**
+// 	**		> Initial Version																**
+// 	**																						**
+// 	**																						**
+// 	**																						**
+// 	******************************************************************************************
+
 package tradingPlatform.exceptions;
 
+/**
+ * Exception thrown when an invalid asset field is inputted
+ */
 public class InvalidAssetException extends Exception {
+    /**
+     * Exception thrown when an invalid asset field is inputted
+     * @param message Error message
+     */
     public InvalidAssetException(String message) {
         super(message);
     }
--- src/tradingPlatform/exceptions/UnitException.java
+++ src/tradingPlatform/exceptions/UnitException.java
@@ -1,6 +1,33 @@
+// 	******************************************************************************************
+// 	**																						**
+// 	**	Filename: UnitException.java  	    												**
+// 	**																						**
+// 	**	Description:																		**
+// 	**																						**
+// 	**																						**
+// 	**	Contributors: Nicole Truong															**
+// 	**																						**
+// 	**																						**
+// 	**	Date Created:																		**
+// 	**																						**
+// 	**																						**
+// 	**	Change Documentation																**
+// 	**		> Initial Version																**
+// 	**																						**
+// 	**																						**
+// 	**																						**
+// 	******************************************************************************************
+
 package tradingPlatform.exceptions;
 
+/**
+ * Exception is thrown when unintended discrepancies occur when manipulating a unit object
+ */
 public class UnitException extends Exception {
+    /**
+     * Exception is thrown when unintended discrepancies occur when manipulating a unit object
+     * @param message Error message
+     */
     public UnitException(String message) {
         super(message);
     }
